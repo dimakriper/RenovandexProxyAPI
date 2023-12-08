69be46e0e3ac4a9480d436838c276e5063aa319a
= <=>
2023-12-08 13:16:55 +0300

    add booking

---
+++ Booking.cs
@@ -0,0 +1,87 @@
+﻿namespace RenovandexProxyAPI
+{
+    // Booking.cs
+
+    public class Booking
+    {
+        public string BookingId { get; set; }
+        public int CompanyId { get; set; }
+        public string UserPhone { get; set; }
+        public int ServiceId { get; set; }
+        public int ResourceId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Comment { get; set; }
+
+        // Other properties as needed
+
+        // Additional methods or properties can be added based on the specific requirements
+    }
+    // BookingInput.cs
+
+    public class BookingInput
+    {
+        public int CompanyId { get; set; }
+        public string UserPhone { get; set; }
+        public int ServiceId { get; set; }
+        public int ResourceId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Comment { get; set; }
+
+        // Other properties as needed
+    }
+    // BookingUpdate.cs
+
+    public class BookingUpdate
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Comment { get; set; }
+
+        // Other properties as needed
+    }
+
+    public class BookingRepository
+    {
+        // Your data access methods go here
+
+
+        public Booking CreateBooking(BookingInput bookingInput)
+        {
+            // Implementation to create a booking in the database
+            // ...
+
+            return new Booking
+            {
+                // Set properties for the created booking
+                // ...
+            };
+        }
+
+        public Booking GetBookingById(string bookingId)
+        {
+            // Implementation to retrieve a booking from the database by ID
+            // ...
+
+            return null; // Replace with actual booking object or null if not found
+        }
+
+        public Booking UpdateBooking(string bookingId, BookingUpdate bookingUpdate)
+        {
+            // Implementation to update a booking in the database
+            // ...
+
+            return null; // Replace with actual updated booking object or null if not found
+        }
+
+        public bool DeleteBooking(string bookingId)
+        {
+            // Implementation to delete a booking from the database
+            // ...
+
+            return false; // Replace with actual result of the deletion operation
+        }
+    }
+
+}
